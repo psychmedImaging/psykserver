b629408d72790faba50c6f5c8b79f7b927a6d359
JonteP <40265580+JonteP@users.noreply.github.com>
2025-03-11 09:55:01 +0100

    bug fixes and cleanup

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -13,10 +13,18 @@ if [ ! -f ${PARTICIPANT_FILE} ]; then
     exit 1
 fi
 
-MRIQC_CONTAINER=mriqc-23.1.1.fix
-FMRIPREP_CONTAINER=fmriprep-23.2.0
-QSIPREP_CONTAINER=qsiprep-0.20.0
+## Editable settings ##
+
+MRIQC_CONTAINER=mriqc-24.0.2
+FMRIPREP_CONTAINER=fmriprep-24.1.1
+QSIPREP_CONTAINER=qsiprep-0.21.4
 XCPD_CONTAINER=xcp_d-0.6.1
+
+MEM_LIMIT=30  # GB
+MAX_THREADS=8 # across all processes
+MAX_TPP=8     # max threads per process
+
+## ----------------- ##
 
 #Globals for sbatch and singularity:
 export BIDS_DIR="${STUDY}/data"
@@ -30,6 +38,9 @@ FREESURFER_DIR="${FMRIPREP_DIR}/sourcedata/freesurfer"
 CONTAINER_DIR="/proj/$PROJECT_NAME/bidsflow"
 TEMPLATEFLOW_HOST_HOME="${CONTAINER_DIR}/templateflow"
 
+# Avoid crash due to earlier unfinished Freesurfer runs:
+rm ${FREESURFER_DIR}/*/scripts/*Running*
+
 # The job array will be based on the number of participants defined in the BIDS dataset:
 NSUBS=$(( $( wc -l ${PARTICIPANT_FILE} | cut -f1 -d' ' ) - 1 ))
 echo "Found participant file with $NSUBS participants."
@@ -40,13 +51,11 @@ echo "Found participant file with $NSUBS participants."
 SBATCH_CMD="sbatch --parsable \
                    --account $PROJECT_NAME \
                    --array=1-$NSUBS \
-                   --time=24:00:00 \
-                   -n 1 \
+                   --time=48:00:00 \
+                   -n ${MAX_THREADS} \
                    -p core \
-                   --cpus-per-task=16 \
-                   --mem-per-cpu=4G \
-                   -o ${HOME}/log/%x-%A-%a.out \
-                   -e ${HOME}/log/%x-%A-%a.err"
+                   -o ${BIDS_DIR}/derivatives/log/%x-%A-%a.out \
+                   -e ${BIDS_DIR}/derivatives/log/%x-%A-%a.err"
 
 SINGULARITY_CMD="singularity run \
                 --cleanenv \
@@ -57,22 +66,23 @@ SINGULARITY_CMD="singularity run \
 MRIQC_CMD="${SINGULARITY_CMD} ${CONTAINER_DIR}/${MRIQC_CONTAINER}.simg /data /data/derivatives/${MRIQC_CONTAINER} participant \
 	      -w /work \
 	      -vv \
-	      --omp-nthreads 8 \
-	      --mem 30"
+	      --omp-nthreads ${MAX_TPP} \
+	      --no-sub \
+	      --mem ${MEM_LIMIT}"
  
 FMRIPREP_CMD="${SINGULARITY_CMD} ${CONTAINER_DIR}/${FMRIPREP_CONTAINER}.simg /data /data/derivatives/${FMRIPREP_CONTAINER} participant \
               -w /work/ \
               -vv \
-              --omp-nthreads 8 \
-              --mem 30 \
+              --omp-nthreads ${MAX_TPP} \
+              --mem $((${MEM_LIMIT}*1000)) \
               --skip_bids_validation"
 
 QSIPREP_CMD="${SINGULARITY_CMD} ${CONTAINER_DIR}/${QSIPREP_CONTAINER}.simg /data /data/derivatives/${QSIPREP_CONTAINER} participant \
              -w /work/ \
              -vv \
-             --omp-nthreads 8 \
-             --nthreads 8 \
-             --mem_mb 30000 \
+             --omp-nthreads ${MAX_TPP} \
+             --nthreads ${MAX_THREADS} \
+             --mem_mb $((${MEM_LIMIT}*1000)) \
              --skip_bids_validation \
              --freesurfer_input=/data/${FREESURFER_DIR} \
              --use-syn-sdc \
@@ -82,9 +92,9 @@ QSIPREP_CMD="${SINGULARITY_CMD} ${CONTAINER_DIR}/${QSIPREP_CONTAINER}.simg /data
 XCPD_CMD="${SINGULARITY_CMD} ${CONTAINER_DIR}/${XCPD_CONTAINER}.simg /data/${FMRIPREP_DIR} /data/derivatives/${XCPD_CONTAINER} participant \
           -w /work/ \
           -vv \
-          --nthreads 8 \
-          --omp-nthreads 8 \
-          --mem_gb 30 \
+          --nthreads ${MAX_THREADS} \
+          --omp-nthreads ${MAX_TPP} \
+          --mem_gb ${MEM_LIMIT} \
           --input-type fmriprep \
           --fs-license-file=${APPTAINERENV_FS_LICENSE}"
 
