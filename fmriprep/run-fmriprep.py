5aee4685e81b9154a3e10dc905315e5f4024120f
JonteP <40265580+JonteP@users.noreply.github.com>
2025-10-08 10:16:09 +0200

    generalise script; add performance plot

--- fmriprep/run-fmriprep.py
+++ fmriprep/run-fmriprep.py
@@ -2,15 +2,14 @@
 #All participants in data/participants.tsv will be processed
 #The script looks for *_config.json in [basefolder]. If not present, default settings are used.
 
-import os, subprocess, argparse, csv, io, datetime, json
+import os, subprocess, argparse, datetime, json
 from sbatch import sbatch
+from sbatch import get_participants
 
 projectName=os.environ['HOSTNAME'].split('-')[0]
 currentFolder=os.path.realpath(__file__)
 containerFolder=os.path.join(currentFolder,'containers')
 templateflowFolder=os.path.join(currentFolder,'templateflow')
-os.environ['APPTAINERENV_FS_LICENSE'] = '/sw/apps/freesurfer/7.4.1/bianca/license.txt'
-os.environ['APPTAINERENV_TEMPLATEFLOW_HOME'] ='/templateflow'
 
 #read arguments and set up paths
 parser = argparse.ArgumentParser(prog='run-fmriprep')
@@ -27,30 +26,25 @@ if not os.path.exists(configPath):
     configPath=os.path.join(currentFolder,configFile)
 with open(configPath) as f:
     cfg=json.load(f)
-    cfgString=' '.join(x + ' ' + y for x, y in cfg['options'].items())
-containerFile='fmriprep-'+cfg['version']
+optionString=' '.join(x + ' ' + y for x, y in cfg['options'].items())
+containerFile=cfg['container']
 logFolder=os.path.join(studyFolder,'logs',containerFile+'_'+datetime.datetime.now().strftime('%Y%m%d-%H%M%S'))
 os.makedirs(logFolder,exist_ok=True)
+for key,val in cfg['environment'].items():
+    os.environ[key] = val
 
 #get participants to process
-participantFile=os.path.join(bidsFolder,'participants.tsv')
-if not os.path.exists(participantFile):
-    raise Exception('No participants.tsv found in '+bidsFolder)
-participants=[]
-with io.open(participantFile,'r',encoding='utf-8-sig') as file:
-    reader=csv.DictReader(file,delimiter='\t')
-    for row in reader:
-        participants.append(row['participant_id'])
+participants=get_participants(bidsFolder)
 array='1-'+str(len(participants))
-
 
 #build the command to send to sbatch:
 getsubjectCommand='subject="$(cut -d" " -f$SLURM_ARRAY_TASK_ID <<<'+'"'+(' '.join(participants))+'")"'
 logCommand='exitcode=$?\necho "$subject\t$SLURM_ARRAY_TASK_ID\t$exitcode" >> '+os.path.join(logFolder,containerFile+'.tsv')
 singularityCommand='singularity run --cleanenv -B '+bidsFolder+':/data -B '+templateflowFolder+':/templateflow -B '+os.environ['TMPDIR']+':/work'
-fmriprepCommand=os.path.join(containerFolder,containerFile)+'.simg /data /data/derivatives/'+containerFile+' participant --participant-label $subject '+cfgString
-fullCommand=getsubjectCommand+'\n'+singularityCommand+' '+fmriprepCommand+'\n'+logCommand
+bidsappCommand=os.path.join(containerFolder,containerFile)+'.simg /data /data/derivatives/'+containerFile+' participant --participant-label $subject '+optionString
+fullCommand=getsubjectCommand+'\n'+singularityCommand+' '+bidsappCommand+'\n'+logCommand
 
 #submit the job array to sbatch:
-sbatch(containerFile,projectName,array,os.path.join(logFolder,'%A-%a'),fullCommand)
-print('\tContainer\t'+os.path.basename(containerFile)+'\n\tProject folder\t'+studyFolder+'\n\tConfig file\t'+configPath+'\n\t# participants\t'+str(len(participants)))
+jobid=sbatch(containerFile,projectName,'11',os.path.join(logFolder,'%A-%a'),fullCommand)
+os.system('cd '+logFolder+' & jobstats --plot -r '+jobid)
+print('Submitted sbatch job '+jobid+'\n\tContainer\t'+os.path.basename(containerFile)+'\n\tProject folder\t'+studyFolder+'\n\tConfig file\t'+configPath+'\n\t# participants\t'+str(len(participants)))
--- fmriprep/sbatch.py
+++ fmriprep/sbatch.py
@@ -1,6 +1,15 @@
-import subprocess
+import subprocess, os, io, csv
 def sbatch(job_name,proj_name,array,log,command,time='24:00:00',threads='8'):
-    sbatch_command = "sbatch -J {} -A {} -a {} -t {} -n {} -o {}.out -e {}.err --wrap='{}'".format(job_name,proj_name,array,time,threads,log,log,command)
-    print(sbatch_command)
-    sbatch_response = subprocess.getoutput(sbatch_command)
-    print(sbatch_response)
+    sbatch_command = "sbatch --parsable -J {} -A {} -a {} -t {} -n {} -o {}.out -e {}.err --wrap='{}'".format(job_name,proj_name,array,time,threads,log,log,command)
+    return subprocess.getoutput(sbatch_command)
+
+def get_participants(folder):
+    file=os.path.join(folder,'participants.tsv')
+    if not os.path.exists(file):
+        raise Exception('No participants.tsv found in '+folder)
+    participants=[]
+    with io.open(file,'r',encoding='utf-8-sig') as f:
+        reader=csv.DictReader(f,delimiter='\t')
+        for row in reader:
+            participants.append(row['participant_id'])
+    return participants
